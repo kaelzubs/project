c283dd1cba166449b1b63650ec87de6452c44cac
kaelzubs <ngmycar>
2021-04-07 21:22:49 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Header from './components/Header';
-import Breadcrumbs from './components/Breadcrumbs';
 import TopHeader from './components/TopHeader';
 import Carousel from './components/Carousel';
 
